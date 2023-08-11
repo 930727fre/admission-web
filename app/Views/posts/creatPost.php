a28da4e03a180f7ecf2df8d2aa6df7e988db98a4
pontusjacky <96285@ms1.phes.chc.edu.tw>
2023-08-11 22:23:02 +0800

    sort out

--- app/Views/posts/creatPost.php
+++ app/Views/posts/creatPost.php
@@ -1,7 +1,10 @@
 <div style = "text -align : center">
     <h1>上傳文章</h1>
     <form action="/ReviseController/gradeStore" enctype="mutipart/form-data" method="POST">
-        <span>標題：</span> <input id = "title" disabled value="請輸入內容"> <input type = "button" onclick = "revise_title()" id = "title_button" value = "修改"><br>
+        <span>標題：</span> <input id = "title" disabled value="請輸入內容"> 
+        <input type = "button" onclick = "revise_title()" id = "title_button" value = "修改">
+        <span class='confirm_button'></span>
+        <br>
         <span>內文：</span> <textarea id = "content" rol="100" col="200" disabled> </textarea>
         <br>
         <div style="height:20px"></div>
@@ -13,10 +16,14 @@
     {
         let title = document.getElementById("title");
         let title_button = document.getElementById("title_button");
+        var confirm_button = document.querySelector('.confirm_button');
+        var element =document.createElement('input');
         if (title_button.value=="修改")
         {
             title_button.value="儲存";
             title.disabled = false;
+            element.textContent="取消";
+            confirm_button.appendChild(element);
         }
         else
         {
