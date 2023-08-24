cd1a5fbef79a8bba3fc00f81dc7ec25d4beb48cc
frem2 <930727fre@gmail.com>
2023-08-25 00:21:20 +0800

    add sidebar content

--- app/Views/webframe/sidebar.php
+++ app/Views/webframe/sidebar.php
@@ -4,6 +4,9 @@
         <hr style="color:white;width: 140px;">
         <li><a href="/"><button type="button" class="btn btn-primary" style="font-size: 20px;">回首頁</button></a></li>
         <li><a href="#">訊息公告</a></li>
+        <li><a href="#">成績上傳</a></li>
+        <li><a href="#">個資修改</a></li>
+        <li><a href="#">公告上傳</a></li>        
         <li><a href="/PostController/law">法令規章</a></li>
         <li><a href="#">重要時程</a></li>
         <li><a href="/PostController/stats">統計數據</a></li>
