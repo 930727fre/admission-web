d331b4536c489ea4cae36e8bfa10cdbe108b8838
lightingboy0412 <lbwang0412@gmail.com>
2023-08-24 23:53:02 +0800

    push

--- app/Views/webframe/sidebar.php
+++ app/Views/webframe/sidebar.php
@@ -2,11 +2,11 @@
 <ul class="side">
         <li class="sidetop">功能列表</li>
         <hr style="color:white;width: 140px;">
-        <li><a href="http://localhost:8080/PostController/index"><button type="button" class="btn btn-primary" style="font-size: 20px;">回首頁</button></a></li>
+        <li><a href="/PageController/index"><button type="button" class="btn btn-primary" style="font-size: 20px;">回首頁</button></a></li>
         <li><a href="#">訊息公告</a></li>
-        <li><a href="http://localhost:8080/PostController/law">法令規章</a></li>
+        <li><a href="/PageController/law">法令規章</a></li>
         <li><a href="#">重要時程</a></li>
-        <li><a href="http://localhost:8080/PostController/stats">統計數據</a></li>
-        <li><a href="http://localhost:8080/PostController/downloads">下載專區</a></li>
+        <li><a href="/PageController/stats">統計數據</a></li>
+        <li><a href="/PageController/downloads">下載專區</a></li>
         <li><a href="#">相關網站</a></li>
 </ul>
