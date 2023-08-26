5e537960e26523ca95c2fd275d7d993b25430e9d
pontusjacky <96285@ms1.phes.chc.edu.tw>
2023-08-26 23:31:59 +0800

    some template use in sign

--- app/Views/uploads/uploadForm.php
+++ app/Views/uploads/uploadForm.php
@@ -1,6 +1,6 @@
 <div style = "text -align : center">
 
-    <form method="post" action="UploadController/upload" enctype="multipart/form-data">
+    <form method="post" action="upload" enctype="multipart/form-data">
         <span>學校：</span> <input name="school" type="text" required><br>
         <span>科系：</span> <input name="department" type="text" required><br>
         <span>檔案上傳：</span><input type="file" name="userfile" required/><br>
